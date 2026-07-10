6d96833523487849408c434b2b5912a875e9a34b
shachar-Panhi <.>
2026-07-10 10:33:45 +0300

    Added Client and Server (currently not asynchronous :( )

--- client.cpp
+++ client.cpp
@@ -1,8 +1,76 @@
-#include <array>
 #include <boost/asio.hpp>
 #include <iostream>
+#include <print>
 
 using boost::asio::ip::tcp;
 
+bool connect_client(tcp::resolver&, tcp::socket&, boost::system::error_code&);
+bool send_message(tcp::socket&, boost::system::error_code&);
+bool receive_print_reply(tcp::socket&, boost::system::error_code&);
+
 int main() {
+    boost::asio::io_context io_context;
+    boost::system::error_code ec;
+    tcp::resolver resolver(io_context);
+    tcp::socket socket(io_context);
+
+    if (!connect_client(resolver, socket, ec))
+        return 1;
+   
+    if (!send_message(socket, ec))
+        return 1;
+
+    if (!receive_print_reply(socket, ec))
+        return 1;
+
+    return 0;
 }
+
+ bool connect_client(tcp::resolver& resolver, tcp::socket& socket, boost::system::error_code& ec) {
+    auto endpoints = resolver.resolve("127.0.0.1", "8080", ec);
+
+    if (ec) {
+        std::print("Resolver error encountered: {}", ec.message());
+        return false;
+    }
+
+    boost::asio::connect(socket, endpoints, ec);
+
+    if (ec) {
+        std::print("Connection failed: {}", ec.message());
+        return false;
+    }
+
+    std::print("Successfully connected!\n");
+    return true;
+ }
+
+ bool send_message(tcp::socket& socket, boost::system::error_code& ec) {
+     std::print("Please enter a message: \n");
+     std::string user_input;
+     std::getline(std::cin, user_input);
+
+     boost::asio::write(socket, boost::asio::buffer(user_input), ec);
+     if (ec) {
+         std::print("Send failed: {}\n", ec.message());
+         return false;
+     }
+
+     std::print("Sent: {}\n", user_input);
+     return true;
+ }
+
+ bool receive_print_reply(tcp::socket& socket, boost::system::error_code& ec) {
+     char reply[1024];
+
+     size_t reply_length = socket.read_some(boost::asio::buffer(reply), ec);
+
+     if (ec) {
+         std::print("Receive failed: {}\n", ec.message());
+         return false;
+     }
+
+     std::string_view received_message(reply, reply_length);
+     std::print("Received back: {}\n", received_message);
+     return true;
+ }
--- echo-server.cpp
+++ echo-server.cpp
@@ -1,8 +1,72 @@
-#include <array>
-#include <iostream>
 #include <boost/asio.hpp>
+#include <iostream>
+#include <print>
 
 using boost::asio::ip::tcp;
 
+bool server_listen(tcp::acceptor&, tcp::endpoint&, boost::system::error_code&);
+void establish_connection(tcp::acceptor&, tcp::socket&, boost::system::error_code&);
+void read_return_data(tcp::socket&, boost::system::error_code&);
+
 int main() {
+    boost::asio::io_context io_context;
+    boost::system::error_code ec;
+    tcp::acceptor acceptor(io_context);
+    tcp::endpoint endpoint(boost::asio::ip::make_address("127.0.0.1"), 8080);
+
+    if (!server_listen(acceptor, endpoint, ec))
+        return 1;
+
+    while (true) {
+        tcp::socket socket(io_context);
+        establish_connection(acceptor, socket, ec);
+
+        if (!ec) {
+            read_return_data(socket, ec);
+        }
+    }
+    return 0;
+}
+
+bool server_listen(tcp::acceptor& acceptor, tcp::endpoint& endpoint,
+                   boost::system::error_code& ec) {
+    acceptor.open(endpoint.protocol(), ec);
+    acceptor.bind(endpoint, ec);
+    acceptor.listen(boost::asio::socket_base::max_listen_connections, ec);
+
+    if (ec) {
+        std::print("Failed to start server: {}", ec.message());
+        return false;
+    }
+
+    std::print("Server is listening on port 8080\n");
+    return true;
+}
+
+void establish_connection(tcp::acceptor& acceptor, tcp::socket& socket,
+                          boost::system::error_code& ec) {
+    acceptor.accept(socket, ec);
+
+    if (ec) {
+        std::print("Connection failed: {}\n", ec.message());
+    } else {
+        std::print("Client successfully connected!\n");
+    }
+}
+
+void read_return_data(tcp::socket& socket, boost::system::error_code& ec) {
+    char data[1024];
+    while (true) {
+        size_t length = socket.read_some(boost::asio::buffer(data), ec);
+
+        if (ec == boost::asio::error::eof) {
+            std::print("Client disconnected without an error\n");
+            break;
+        } else if (ec) {
+            std::print("Data error: {}\n", ec.message());
+            break;
+        }
+
+        boost::asio::write(socket, boost::asio::buffer(data, length), ec);
+    }
 }
